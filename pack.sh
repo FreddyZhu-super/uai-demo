e2556a5977bf289ef13c7e095c2f8b30b187d06f
Freddy <13526558229@163.com>
2020-03-06 10:55:48 +0800

    v1.3

--- pack.sh
+++ pack.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-sudo python tf_tool.py pack --public_key='oTIvrnyeuBra__9MNUJxOVGbzlpB94z5vOSln5Fk' \
---private_key='nP8zQj-ILqxk0-OiwCfFUI8eQoIzYe7-X2Tpjan3jzX38kxZ_Tc-mBCk3y89-Tjl' \
+sudo python tf_tool.py pack --public_key='' \
+--private_key='' \
 --code_path='./mnist/code' \
 --mainfile_path='mnist.py' \
---uhub_username='freddy.zhu@ucloud.cn' \
---uhub_password='FREDDY.zhu12' \
---uhub_registry='uai_demo_fred' \
---uhub_imagename='mnist_fred' \
---uhub_imagetag='demo_1.0' \
---test_data_path='./mnist/data' \
+--uhub_username='' \
+--uhub_password='' \
+--uhub_registry='' \
+--uhub_imagename='' \
+--uhub_imagetag='' \
+--test_data_path='./mnist/data/' \
 --test_output_path='./mnist/models/' \
 --ai_arch_v='tensorflow-1.1.0' \
 --internal_uhub=false
--- pack_inference.sh
+++ pack_inference.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-sudo python uai_tool.py packdocker tf --public_key='oTIvrnyeuBra__9MNUJxOVGbzlpB94z5vOSln5Fk' \
---private_key='nP8zQj-ILqxk0-OiwCfFUI8eQoIzYe7-X2Tpjan3jzX38kxZ_Tc-mBCk3y89-Tjl' \
+sudo python uai_tool.py packdocker tf --public_key='' \
+--private_key='' \
 --main_class='MnistModel' \
 --main_module='mnist_inference' \
 --model_dir='checkpoint_dir' \
 --pack_file_path='./mnist_inference' \
---uhub_username='freddy.zhu@ucloud.cn' \
---uhub_password='FREDDY.zhu12' \
---uhub_registry='uai_demo_fred' \
---uhub_imagename='mnist_inference_fred' \
+--uhub_username='' \
+--uhub_password='' \
+--uhub_registry='' \
+--uhub_imagename='' \
 --ai_arch_v='tensorflow-1.1.0' \
 --in_host=no
 
